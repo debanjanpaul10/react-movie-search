62bd71b1e10a7edb78cc263e2d52bc8f60464538
Debanjan Paul <debanjanpaul10@gmail.com>
2021-02-27 15:39:34 +0530

    Production build

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./Components/Header"
+import SearchMovies from "./Components/SearchMovies"
+
+function App() {
+    return (
+        <div className="container">
+            <Header />
+            <SearchMovies />
+        </div>
+    )
+}
+
+export default App;
